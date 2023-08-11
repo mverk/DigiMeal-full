4baf4654f52741bf05a662dbb9f7aa7cc2822933
mverk <66826581+AkaMikeee@users.noreply.github.com>
2023-08-11 12:45:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log('DigiCash started');
+
+function openMenu() {
+    const squareWindow = document.querySelector('.square-window');
+    squareWindow.style.opacity = 1;
+}
+
+function closeMenu() {
+    const squareWindow = document.querySelector('.square-window');
+    squareWindow.style.opacity = 0;
+}
